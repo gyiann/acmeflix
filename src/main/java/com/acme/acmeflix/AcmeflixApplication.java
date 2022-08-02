031f05f7cfcabc7e538f5d30bdabcdcecbe1227d
g.giannakis <g.giannakis@pmmretail.com>
2022-08-02 17:11:04 +0300

    Initial commit.

---
+++ src/main/java/com/acme/acmeflix/AcmeflixApplication.java
@@ -0,0 +1,13 @@
+package com.acme.acmeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/acmeflix/AcmeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.acmeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
