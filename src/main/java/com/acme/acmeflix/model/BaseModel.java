c16b79a6126d17dc2b44f3812b0b4a96cb9fd9ab
g.giannakis <g.giannakis@pmmretail.com>
2022-08-04 16:09:26 +0300

    Create Base Model.

---
+++ src/main/java/com/acme/acmeflix/model/BaseModel.java
@@ -0,0 +1,15 @@
+package com.acme.acmeflix.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public abstract class BaseModel {
+
+    private Long id;
+}
